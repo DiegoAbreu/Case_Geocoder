02a58f93f771c38e13ca602d56f94fa8807d3f31
DiegoAbreu <diego.souza.abreu@gmail.com>
2020-05-07 00:10:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Portal Oi de Geocodificação - Caso de Uso: 
+
 # Pacotes
 import streamlit as st
 import pandas as pd
@@ -6,7 +8,8 @@ import plotly.express as px
 import geopy.distance
 
 # Título:
-st.title('Caso de uso de Geocodificação:')
+st.image('https://www.oi.com.br/empresas/publicV2/brand-oi.svg', use_column_width=False)
+st.title('Caso de uso do Portal Oi de Geocodificação:')
 st.header('Aplicação para encontrar endereços mais próximos')
 
 # Recursos
@@ -14,34 +17,25 @@ recursos_df = pd.read_csv('recursos.csv', sep=';', encoding = "ISO-8859-1")
 recursos_df['Localidade'] = 'Recursos'
 recursos_df['Bolha'] = 0.05
 
-# Mostrar tabela de recursos
-#st.write('Esta é nossa lista de pontos de recursos:')
-st.text("Um exemplo bastante comum de uso de dados geocodificados é o cálculo de distância \nentre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que \nos usuários possam pesquisar e encontrar locais próximos.")
-st.text("Esta é uma lista de 100 escolas públicas do Rio de Janeiro")
-
-
-tabela = pd.DataFrame(recursos_df['Ponto'])
-tabela
-
-
-st.text("Um exemplo bastante comum de uso de dados geocodificados é o cálculo de distância \nentre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que \nos usuários possam pesquisar e encontrar locais próximos.")
-st.text("Esta é uma lista de 100 escolas públicas do Rio de Janeiro")
+# Introdução
+st.markdown("Um uso bastante comum de dados geocodificados é o cálculo de distância entre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que os usuários possam pesquisar e encontrar locais próximos.")
+st.markdown("")
+st.write('Veja o exemplo a seguir:')
 
 # Plot Recursos
 import plotly.express as px
-fig = px.scatter_mapbox(recursos_df, lat="Latitude", lon="Longitude", hover_name="Ponto", zoom=10, height=500)
-fig.update_layout(mapbox_style="carto-positron")
-fig.update_layout(margin={"r":0,"t":25,"l":0,"b":0})
-#fig.show()
-st.plotly_chart(fig)
+fig1 = px.scatter_mapbox(recursos_df, lat="Latitude", lon="Longitude", hover_name="Ponto", zoom=10, height=500)
+fig1.update_layout(mapbox_style="carto-positron")
+fig1.update_layout(margin={"r":0,"t":25,"l":0,"b":0})
+st.plotly_chart(fig1)
 
-st.text("Um exemplo bastante comum de uso de dados geocodificados é o cálculo de distância \nentre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que \nos usuários possam pesquisar e encontrar locais próximos.")
-st.text("Esta é uma lista de 100 escolas públicas do Rio de Janeiro")
+st.markdown("No mapa acima temos a localização de 100 escolas públicas da cidade do Rio de Janeiro.Insira um endereço no campo a a seguir e descubra quais escolas mais próximas.")
 
 # Endereço para geocodificar:
-endereco_usuario = st.text_input('Insira Aqui seu endereço','')
+endereco_usuario = st.text_input('Faça sua busca:','')
+
 # Token:
-Token_ = st.sidebar.text_input('Insira o token do Portal de geocodificação','')
+Token_ = st.sidebar.text_input('Insira o token do Portal Oi de Geocodificação','')
 
 def consulta(x):
     if x == '':
@@ -51,8 +45,8 @@ def consulta(x):
         headers = {'accept': '*/*','Authorization': Token_}
         params = (('endereco', x),)
         response = requests.get('https://geosite.oi.net.br/geocodificacao-api/api/v1/geocode/json', headers=headers, params=params)
+        
         # Resultados:
-        #resultado = response.json()
         resultado = pd.DataFrame(response.json())
         Loc_usuario = pd.DataFrame()
         Loc_usuario['Ponto'] = resultado['endereco']
@@ -68,39 +62,62 @@ def consulta(x):
         ref_usuario = list(zip(Loc_usuario['Latitude'], Loc_usuario['Longitude']))
         lat_long = list(zip(usu_recs['Latitude'], usu_recs['Longitude']))
 
-
         # Distancia ref_usu
         list_dist = list(map(lambda x: round(geopy.distance.distance(x, ref_usuario).km,3), lat_long))
-        usu_recs["distancia_da_ref(km)"] = list_dist
+        usu_recs["Distancia(km)"] = list_dist
+        usu_recs["Distancia(km)"] = usu_recs["Distancia(km)"].round(2)
         usu_recs = usu_recs.reset_index(drop=True)
-        usu_recs = usu_recs.sort_values(by=['distancia_da_ref(km)'])
+        usu_recs = usu_recs.sort_values(by=['Distancia(km)'])
         usu_recs = usu_recs.reset_index(drop=True)
-
+        usu_recs["Distancia(km)"] = usu_recs["Distancia(km)"].astype(str)
+        usu_recs["Distancia(km)"] = usu_recs["Distancia(km)"].replace('00','')
 
         # Tag top 5
-        usu_recs.loc[usu_recs.index <= 5, 'Localidade'] = 'Recursos mais próximos' 
+        usu_recs.loc[usu_recs.index <= 5, 'Localidade'] = 'Escolas mais próximas' 
+        usu_recs.loc[usu_recs.index > 5, 'Localidade'] = 'Escolas' 
         usu_recs.loc[usu_recs.index == 0, 'Localidade'] = 'Você' 
-
+        usu_recs = usu_recs.rename(columns={'Ponto': 'Endereço', 'Localidade': 'Ponto' })
 
         # Top 5 mais próximas
-        st.write('Esses são os 5 pontos mais próximos:')
-        st.text("Um exemplo bastante comum de uso de dados geocodificados é o cálculo de distância \nentre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que \nos usuários possam pesquisar e encontrar locais próximos.")
-        st.text("Esta é uma lista de 100 escolas públicas do Rio de Janeiro")
-        # Plot
-        import plotly.express as px
-        fig = px.scatter_mapbox(usu_recs, lat="Latitude", lon="Longitude", hover_name="Ponto", zoom=10, height=500, color="Localidade", size= 'Bolha')
-        fig.update_layout(mapbox_style="carto-positron")
-        fig.update_layout(margin={"r":0,"t":25,"l":0,"b":0})
-        fig.update_layout({'legend_orientation':'h'})
-        #fig.show()
-        st.plotly_chart(fig)
+        st.markdown("***")
+        st.write('Essas são as 5 escolas mais próximas do seu endereço:')
+
         # DataFrame Top5
         top_5 = (usu_recs.loc[1:]).head()
+        top_5 = top_5[['Endereço', 'Ponto','Distancia(km)']]
         top_5
+        st.markdown("Veja no Mapa:")
 
-        st.text("Um exemplo bastante comum de uso de dados geocodificados é o cálculo de distância \nentre dois ou mais pontos. Diversos sites utilizam esse recurso para permitir que \nos usuários possam pesquisar e encontrar locais próximos.")
-        st.text("Esta é uma lista de 100 escolas públicas do Rio de Janeiro")
-        usu_recs
+        # Plot
+        import plotly.express as px
+        fig2 = px.scatter_mapbox(usu_recs, lat="Latitude", lon="Longitude", hover_name="Endereço", zoom=10, height=500, color="Ponto", size= 'Bolha')
+        fig2.update_layout(mapbox_style="carto-positron")
+        fig2.update_layout(margin={"r":0,"t":25,"l":0,"b":0})
+        fig2.update_layout({'legend_orientation':'h'})
+        st.plotly_chart(fig2)
+        st.markdown("Confira a lista completa ordenada por proximidade:")
+        lista_completa = usu_recs[['Endereço', 'Ponto', 'Distancia(km)']]
+        lista_completa
+
+        # Texto sobre o uso do Geocoder Oi
+        st.markdown("***")
+        st.header('Onde e como foi utilizado o Portal Oi de Geocodificação nessa aplicação?')
+        st.markdown("A Geocodificação de endereços foi aplicada em dois momentos nessa solução:")
+        st.markdown("**1 - ** Obter os pontos de latitude e longitude dos endereços das escolas. Para isso utilizamos o recurso de processamento em lote do Portal. Onde fazemos o uploadde nossa tabela de endereços e após o processamento recebemos um arquivo com seus respectivos valores de coordenadas.")
+        entrada_basica = recursos_df['Ponto']
+        entrada_basica
+        entrada_latlng = recursos_df[['Ponto', 'Latitude', 'Longitude']]
+        entrada_latlng
+        st.markdown("Com essas informações de Latitude e Longititude agora disponíveis é possivel exibir os endereços graficamente como pontos em uma mapa.")
+        st.plotly_chart(fig1)
+        st.markdown("**2 - ** A segunda geocodificação ocorre sempre que um usuário pesquisa por um endereço. Para essa etapa utilizamos o recurso de consumo via API que nos permite geocodificar fora da Portal, ou seja dentro de nossa aplicação.")
+        st.markdown("Com a latitude e longitude do endereço pesquisado, podemos então calcular as distâncias entre os pontos, ordená-los e apresentar o resultado final.")
+        st.plotly_chart(fig2)
+        
+        # Texto sobre valor
+        st.markdown("***")
+        #st.header('O valor e as possibilidades desse tipo de Aplicação')
+
         return
 consulta(endereco_usuario)
 
